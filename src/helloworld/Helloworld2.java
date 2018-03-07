a2fc267578721b8862fcddcfa25ae4be71c0509a
danicervantes <dancervantes20@gmail.com>
2018-03-07 13:55:35 -0800

    mensaje 3

--- src/helloworld/Helloworld2.java
+++ src/helloworld/Helloworld2.java
@@ -4,7 +4,7 @@ public class Helloworld2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Wasaaaap");
+		System.out.println("Hola");
 
 	}
 
